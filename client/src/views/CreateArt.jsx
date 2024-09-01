41be381c4fe0f2506f55ef1e76f26d1621f59ad3
Charle Cho <charlexcho@gmail.com>
2024-09-01 00:15:35 -0700

    Charle's commit

---
+++ client/src/views/CreateArt.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const CreateArt = () => {
+  return (
+    <>
+    <h1>Create</h1>;
+    </>
+)};
+
+export default CreateArt;
---
+++ client/src/views/EditArt.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const EditArt = () => {
+  return (
+    <>
+    <h1>Update</h1>;
+    </>
+)};
+
+export default EditArt;
